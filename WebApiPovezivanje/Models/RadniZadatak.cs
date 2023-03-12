84855ba193dbdce853b1e350947602b6701f1060
Milan <milanjurjevic@hotmail.com>
2023-03-12 14:13:41 +0100

    komit

---
+++ WebApiPovezivanje/Models/RadniZadatak.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System.Runtime.CompilerServices;
+
+namespace WebApiPovezivanje.Models
+{
+    public class RadniZadatak
+    {
+        public int id { get; set; } 
+        public string todo { get; set; }
+        public string radnikIme { get; set; }
+        public string radnikPrezime { get; set;}
+        public string opisPosla { get; set; }
+        public DateTime? datum { get; set; }
+        public bool isDone { get; set; }
+       
+
+    }
+}
